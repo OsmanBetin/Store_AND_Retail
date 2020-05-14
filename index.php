35a864cfcd5b4d5eca767845453386f9f80f8bb1
Osman Fernandez <investigadorbiotec@gmail.com>
2020-05-13 21:55:33 -0500

    Historia001_Index:Borrardor Index conectar con registro de Usuarios

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Store & Retail</title>
+  </head>
+  <body>
+    <h1>Store & Retail</h1>
+
+    <div class="">
+      <form class="" action="index.html" method="post">
+        <table>
+          <tr>
+            <td>Buscar<input type="text" name="" value=""></td>
+            <td>Registrarme<input type="button" name="registrar" value="Aquí"></td>
+            <td>Carrito<input type="button" name="carrito" value="Agregar"></td>
+          </tr>
+        </table>
+      </form>
+    </div>
+
+    <div class="">
+      <h1>Productos en Oferta para tí</h1>
+      <p><strong>Store & Retal</strong> siempre piensa en tí.
+        Por eso te te brinda la oportunidad de adquirar tus Productos
+        de preferencias con grandes descuentos.</p>
+        <ul>
+          <li>Bebidas alcoholicas</li>
+          <li>Bebidas No alcoholicas</li>
+        </ul>
+    </div>
+
+    <div class="">
+      <h1>Mostrar Aquí todos los Productos</h1>
+      <ul>
+        <li>Canasta Familiar</li>
+        <li>Productos de Aseo</li>
+        <li>Carnes Frías</li>
+      </ul>
+    </div>
+
+    <div class="">
+
+      <p>
+        Estamos Ubicados en la Ciudad de Medellín.
+        Cerca de la Universidad de Antioquia.
+      </p>
+
+      <p>Redes Sociales</p>
+      <p>Incluir <i>Whatsapp</i></p>
+      <p>Incluir aquí <i>Facebook</i></p>
+      <p>Incluir aquí <i>Twiter</i></p>
+    </div>
+  </body>
+</html>
