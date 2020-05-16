4743792dbb7a2d93312760ab7751e49f0bf28b00
Osman Fernandez <investigadorbiotec@gmail.com>
2020-05-16 15:47:47 -0500

    Historia002_RegistroUsuarios:Ingresa Usuarios a la BD Tienda-Tabla Cliente

---
+++ form_registrar_users.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="">
+      <h1>Store & Retail</h1>
+
+      <p>Quiero Registrarme!</p>
+    </div>
+
+    <div class="">
+
+      <form class="" action="registrar_users.php" method="post">
+
+        <label for="ide">Identificación<input type="text" name="ide" id="ide" value=""></label><br>
+        <label for="nombre">Nombre<input type="text" name="nombre" id="nombre" value=""></label><br>
+        <label for="apellido">Apellido<input type="text" name="apellido" id="apellido" value=""></label><br>
+        <label for="fecNacimiento">Fecha de Nacimiento<input type="text" name="fecNacimiento"  id="fecNacimiento" value=""></label><br>
+        <label for="barrio">Barrio<input type="text" name="barrio" id="barrio" value=""></label><br>
+        <label for="email">E-mail<input type="text" name="email" id="email" value=""></label><br>
+
+        <input type="submit" name="enviando" value="Guardar"><br>
+
+      </form>
+
+    </div>
+
+
+
+  </body>
+</html>
