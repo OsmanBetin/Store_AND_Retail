0d497ded7b8188ec76ab1cf48d3a9a663c181535
Osman Fernandez <investigadorbiotec@gmail.com>
2020-05-25 22:31:53 -0500

    Historia004_FormularioInterCliente:Ingresar Usr y verificar si está en la BD

---
+++ form_InterCliente.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <div class="">
+      <h1>Store & Retail</h1>
+
+      <p>Quiero Realizar un Pedido!</p>
+      <p>Sin embargo... Primero debemos verificar si el Usr
+      está Registrado en Nuestra Base de Datos.</p>
+    </div>
+
+    <div class="">
+      <form class="" action="verificarCliente.php" method="post">
+        <table>
+          <tr>
+            <td>Ingrese Id Cliente <input type="text" name="id_cliente" id="id_cliente"> </td>
+            <td><input type="submit" name="id_cliente" id="id_cliente" value="Enviar"></td>
+          </tr>
+        </table>
+
+      </form>
+    </div>
+
+  </body>
+</html>
